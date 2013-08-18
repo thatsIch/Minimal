26ba0ec10a3ac04a976a5d86ac04743bfe5121ea
thatsIch <badhard@arcor.de>
2013-08-18 17:51:50 +0200

    retrieving entries complete

--- @Resources/Scripts/DCal/Parser.lua
+++ @Resources/Scripts/DCal/Parser.lua
@@ -1,36 +1,54 @@
---
+-- upon webparser creation
 function Initialize()
+	-- tables
+	T_CAL_RAWFORMAT = {}
+	T_MONTH_NUM = {Jan=1;Feb=2;Mar=3;Apr=4;Mai=5;Jun=6;Jul=7;Aug=8;Sep=9;Okt=10;Nov=11;Dez=12;}
 
-	-- fetch all Calendars
-	local cals = {}
-	local iterator = 1
-	local cal = SKIN:GetVariable("Cal" .. iterator)
-	local mWebParser = SKIN:GetMeasure("mWebParser")
-
-	repeat
-		-- SKIN:Bang('!SetOption', 'mWebParser', 'Url', cal)
-		-- SKIN:Bang('!CommandMeasure', 'mWebParser', 'Update')
-		-- print(mWebParser:GetOption('Url'))		
-		-- print(mWebParser:GetValue())
-		iterator = iterator + 1
-		cal = SKIN:GetVariable("Cal" .. iterator)
-	until not cal
-
-	-- print(mWebParser:GetValue())
-
-	print("parser funzt")
-	-- printTable(cals)
-end
-
-function Update()
-
+	-- pattern
+	PAT_ENTRY = '<entry.-</entry>'
+	PAT_TITLE = '.-<title.->(.-)</title>.-'
+	PAT_DATE = '.-Wann: (.-)&lt'
+	PAT_DATE_TIME = '(%a%a) (%d+)%. (.-)%.? (%d%d%d%d)'
+	PAT_TIME = '(%d%d)%:(%d%d)'
+	
+	
 end
 
 function parseNextCalendar()
+	print("called parseNextCalendar")
+
 	local webParser = SKIN:GetMeasure('mWebParser')
 	local currUrl = webParser:GetOption('Url')
 
-	-- iterate through cal list to determine which ID currently is
+	-- store fetched file path
+	local filePath = webParser:GetStringValue()
+
+	-- check if file exists
+	if not filePath then
+		print("filePath not valid: " .. filePath)
+		return
+	end
+
+	-- open files and fetch data directly since temp files dont survive next cycle
+	table.insert(T_CAL_RAWFORMAT, fetchContent(filePath))
+	
+
+	-- get next Cal
+	local fetchedUrl = fetchNextCalUrl(currUrl)
+
+	-- check for validity
+	if not fetchedUrl then
+		prepareRawData()
+		return
+	end
+
+	-- pass new url and update
+	SKIN:Bang('!SetOption', 'mWebParser', 'Url', fetchedUrl)
+	SKIN:Bang('!CommandMeasure', 'mWebParser', 'Update')
+end
+
+-- needs the current url and fetches the next url in line sorted by Variable ID
+function fetchNextCalUrl(currUrl)
 	local iterator = 1
 	local url = SKIN:GetVariable("Cal" .. iterator)
 
@@ -40,7 +58,40 @@ function parseNextCalendar()
 		url = SKIN:GetVariable("Cal" .. iterator)
 	end
 
+	-- fetch next variable
+	iterator = iterator + 1
+	url = SKIN:GetVariable("Cal" .. iterator)
+
+	return url
+end
+
+-- opens a specified file path
+function fetchContent(filePath)
+	local fileHandle = io.open(filePath)
+	local content = fileHandle:read('*all')
+	io.close(fileHandle)
+
+	return content
+end
+
+function prepareRawData()
+	print("Prepare the raw Data.")
 	
+	if not isTable (T_CAL_RAWFORMAT) then
+		print("T_CAL_RAWFORMAT is not a table.")
+		return
+	end
+
+	for _, rawCals in pairs(T_CAL_RAWFORMAT) do
+		print("RAW CAL")
+		for entry in string.gmatch(rawCals, PAT_ENTRY) do
+
+		end
+	end
+end
+
+function isTable(obj)
+	return (type(obj) == "table")
 end
 
 function printTable(table)

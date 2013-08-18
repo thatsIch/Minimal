7de74edd8ed40d59633ba0c3ee140eca948dde1e
thatsIch <badhard@arcor.de>
2013-08-18 23:59:10 +0200

    fix alignment

--- @Resources/Scripts/DCal/Aligner.lua
+++ @Resources/Scripts/DCal/Aligner.lua
@@ -5,15 +5,28 @@ function Initialize()
 	local DayWidth = SKIN:GetVariable("DayWidth")
 	local DaySpacing = SKIN:GetVariable("DaySpacing")
 
-	local dummy = SKIN:GetMeter("mDummy")
+	local count = getTotalCount()
+	local RealSpacing = ((workareawidth - count * (DayWidth + DaySpacing)) / 2)
 
-	local RealSpacing = ((workareawidth % (DayWidth + DaySpacing)) / 2)
 
-	dummy:SetX(RealSpacing)
+
+	SKIN:GetMeter("mDummy"):SetX(RealSpacing)
 end
 
 -- on each update
 -- shouldnt be called
 function Update()
 
+end
+
+function getTotalCount()
+	local count = 0
+	local elem = ""
+
+	repeat
+		count = count + 1
+		elem = SKIN:GetMeter('mD'.. count ..'Date')
+	until not elem
+
+	return (count - 1)
 end

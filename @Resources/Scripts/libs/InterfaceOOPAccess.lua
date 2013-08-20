053b9e6d0fee44d8a4d65c389cdd367a1bfb567b
thatsIch <badhard@arcor.de>
2013-08-20 19:04:41 +0200

    bugfixes oopaccess

--- @Resources/Scripts/libs/InterfaceOOPAccess.lua
+++ @Resources/Scripts/libs/InterfaceOOPAccess.lua
@@ -2,30 +2,45 @@ return function(SKIN)
 	local ms = {
 		__index = function(table,key) 
 
+			-- catch recursive call		
+			if key == '__section' and #table < 2 then
+				return
+
+			elseif key == '__sectionname' and #table < 2 then
+				return
+
 			-- catch UpdateMeasure()
-			if key == 'UpdateMeasure' then 
+			elseif key == 'UpdateMeasure' then 
 				return function() SKIN:Bang('!UpdateMeasure',table.__sectionname, SKIN:GetVariable('CURRENTCONFIG')) end 
 
 			-- catch UpdateMeter()	
 			elseif key == 'UpdateMeter' then 
 				return function() SKIN:Bang('!UpdateMeter',table.__sectionname, SKIN:GetVariable('CURRENTCONFIG')) end 
 
+			-- catch IsMeter()
+			elseif key == 'isMeter' then
+				return function() return SKIN:GetMeter(table.__sectionname) and true or false end
+
+			-- catch IsMeter()
+			elseif key == 'isMeasure' then
+				return function() return SKIN:GetMeasure(table.__sectionname) and true or false end
+
 			-- catch Rainmeter Native Build-In functions
 			-- Show, Hide, SetXYWH, GetXYWH, GetName, GetOption (though special case), Enable, Disable, GetValueRange, GetRelativeValue, GetMaxValue, 
-			elseif table.__section[key] then 
+			elseif #table > 0 and table.__section[key] then 
 				return function(...) return table.__section[key](table.__section,...) end 
 			
 			-- catch meter options
-			elseif table.__section.GetOption then
+			elseif #table > 0 and table.__section.GetOption then
 				return table.__section:GetOption(key)
 
 			-- catch measure options
-			elseif table.__section.GetNumberOption then
+			elseif #table > 0 and table.__section.GetNumberOption then
 				return (table.__section.GetNumberOption and table.__section:GetNumberOption(key,nil))
 			
 			-- unknown case
 			else
-				print('Unkown case: ' .. key) 
+				print('Unkown Section Case: ' .. key) 
 				return
 			end 
 		end, 
@@ -33,16 +48,26 @@ return function(SKIN)
 	}
 	local sections = {
 		__index = function(table,key) 
-			sections[key] = {}
+			if key == 'Redraw' then
+				return function() SKIN:Bang('!Redraw', SKIN:GetVariable('CURRENTCONFIG')) end
+			else
+				sections[key] = {}
 
-			-- store meter/measure
-			sections[key].__section = SKIN:GetMeasure(key) or SKIN:GetMeter(key)
-			
-			-- store meter/measurename
-			sections[key].__sectionname = key 
-			setmetatable(sections[key],ms) 
+				-- store meter/measure
+				sections[key].__section = SKIN:GetMeasure(key) or SKIN:GetMeter(key)
+				
+				-- store meter/measurename
+				sections[key].__sectionname = key 
+				setmetatable(sections[key],ms) 
 
-			return sections[key] 
+				return sections[key]
+			end
+		end,
+		isMeter = function(meter)
+			return SKIN:GetMeter(meter) and true or false
+		end,
+		isMeasure = function(measure)
+			return SKIN:GetMeasure(measure) and true or false
 		end
 	}
 	local variables = {
@@ -58,7 +83,7 @@ return function(SKIN)
 
 			-- unknown case
 			else 
-				print('Unkown case: ' .. key)
+				print('Unkown Variable Case: ' .. key)
 				return
 			end
 		end, 

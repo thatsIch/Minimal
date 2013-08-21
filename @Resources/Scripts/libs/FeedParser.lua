8a74b516f13185365477d13106f32db2d4fcb44a
thatsIch <badhard@arcor.de>
2013-08-21 09:06:12 +0200

    went back to converttable, seems most reliable

--- @Resources/Scripts/libs/FeedParser.lua
+++ @Resources/Scripts/libs/FeedParser.lua
@@ -1,4 +1,69 @@
 local FeedParser do
+	-- APPLIES A GIVEN CONVERTTABLE ONTO INPUTSTRING
+	-- @input: String string
+	-- @output: String string
+	function StringReplaceByTable(string)
+		-- USED GLOBAL FUNCTIONS
+		local char = string.char
+		local gsub = string.gsub
+
+		if string.find(string, 'Armut') then
+			print(string)
+		end
+		-- CONVERT TABLE
+		local generalConvertTable = {
+			[char(226,128,153)] = char(39), -- '
+			[char(226,128,147)] = char(150), -- thinkline
+			[char(226,128,156)] = char(134), -- bottom "
+			[char(226,128,142)] = '',
+			[char(226,128,158)] = char(147), -- "
+		}
+		
+		-- ITERATE THROUGH ALL ENTRIES AND REPLACE
+		for k,v in pairs(generalConvertTable) do
+			string = gsub(string, k, v)
+		end
+		
+		-- CONVERT TABLE
+		generalConvertTable = {
+			[char(195,164)] = char(228), -- auml
+			[char(195,182)] = char(246), -- ouml
+			[char(195,188)] = char(252), -- uuml
+			[char(195,132)] = char(196), -- Auml
+			[char(195,150)] = char(214), -- Ouml
+			[char(195,156)] = char(220), -- Uuml
+			[char(195,159)] = char(223), -- sz
+			[char(195,32)] = char(32), -- space
+			[char(195,169)] = char(233), -- e tegue
+			[char(226,128)] = char(147), -- top "
+			[char(195,168)] = char(232), -- e grave
+			[char(194)] = '', -- space
+			['&auml;'] = char(228), 
+			['&ouml;'] = char(246), 
+			['&uuml;'] = char(252), 
+			['&Auml;'] = char(196), 
+			['&Ouml;'] = char(228), 
+			['&Uuml;'] = char(214), 
+			['&szlig;'] = char(220), 
+			['&amp;'] = char(38), 
+			['&lt;'] = char(60), 
+			['&gt;'] = char(62), 
+			['&nbsp;'] = char(32), 
+			['&(%w-);'] = '',
+		}
+		
+		-- ITERATE THROUGH ALL ENTRIES AND REPLACE
+		for k,v in pairs(generalConvertTable) do
+			string = gsub(string, k, v)
+		end
+
+		-- SOLE FUNCTION TO REPLACE LUA REPRESENTATION
+		string = gsub(string,"&#(%d+);", function(c) local num = tonumber(c) if 0 <= num and num <= 255 then return char(num) else return "" end end)
+
+		-- RESULT
+		return string
+	end -- StringReplaceByTable
+
 	-- RSS
 	-- ==================================================
 	-- @param entry string
@@ -63,7 +128,7 @@ local FeedParser do
 		string.match( entry, '<content.->(.-)</content>' ) or
 		"(no content)"
 
-		-- REMOVE HTML TAGS OUT OF CONTENT AND SHORTEN IT
+		-- REMOVE HTML TAGS, UTF OUT OF CONTENT AND SHORTEN IT
 		content = string.gsub(content, '<.->', '')
 		content = string.sub(content, 0, 425)
 
@@ -83,11 +148,10 @@ local FeedParser do
 
 		-- RSS
 		for entry in string.gmatch(rawFeed, '<item.->(.-)</item>') do
+			entry = StringReplaceByTable(entry)
 			table.insert(parsedEntries, {
-				-- title = StringReplaceByTable(getRSSTitle(entry));
 				title = getRSSTitle(entry);
 				link = getRSSLink(entry);
-				-- cont = StringReplaceByTable(getRSSContent(entry));
 				cont = getRSSContent(entry);
 				img = getRSSImage(entry);
 			})
@@ -96,11 +160,10 @@ local FeedParser do
 
 		-- ATOM
 		for entry in string.gmatch(rawFeed, '<entry.->(.-)</entry>') do
+			entry = StringReplaceByTable(entry)
 			table.insert(parsedEntries, {
-				-- title = StringReplaceByTable(getAtomTitle(entry));
 				title = getAtomTitle(entry);
 				link = getAtomLink(entry);
-				-- cont = StringReplaceByTable(getAtomContent(entry));
 				cont = getAtomContent(entry);
 				img = getAtomImage(entry);
 			})

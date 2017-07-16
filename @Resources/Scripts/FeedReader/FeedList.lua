0ab08afdd6a915e172d04d4b2011620617aff4cc
thatsIch <thatsIch@mail.de>
2017-07-17 00:52:24 +0200

    Removed unavailable feeds from list

--- @Resources/Scripts/FeedReader/FeedList.lua
+++ @Resources/Scripts/FeedReader/FeedList.lua
@@ -4,9 +4,7 @@
 -- url: url to the feed to be parsed
 -- pin (optional): if true it has higher priority and is as most top as possible
 return {
-	{"Computer", "Rainmeter", "http://rainmeter.net/forum/feed.php", true},
 	{"Nachrichten", "Spiegel Online", "http://www.spiegel.de/schlagzeilen/tops/index.rss", true},
-	{"Videos", "Youtube", "http://gdata.youtube.com/feeds/base/users/badhardba/newsubscriptionvideos", true},
 	{"Videos", "Watchanimeon", "http://www.watchanimeon.com/feed/atom/"},
 	{"Musik", "iTunes Top 25", "http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topsongs/limit=25/xml"},
 	{"Nachrichten", "Die Welt", "http://www.welt.de/?service=Rss"},
